207a9959f3819b771faecf9584b102f1129dd9ae
Your Name <you@example.com>
2025-09-21 18:05:04 -0400

    switch to ytdlp

--- track_downloader.py
+++ track_downloader.py
@@ -2,11 +2,65 @@ import os
 import sys
 import csv
 import requests
+import re
+import yt_dlp
 from configparser import ConfigParser
-from pytube import YouTube
-from pytube.helpers import safe_filename
 from mutagen.easymp4 import EasyMP4
 from mutagen.mp4 import MP4, MP4Cover
+
+
+def safe_filename(s: str, max_length: int = 255) -> str:
+    """Sanitize a string making it safe to use as a filename.
+
+    This function was based off the limitations outlined here:
+    https://en.wikipedia.org/wiki/Filename.
+
+    :param str s:
+        A string to make safe for use as a file name.
+    :param int max_length:
+        The maximum filename character length.
+    :rtype: str
+    :returns:
+        A sanitized string.
+    """
+    # Characters in range 0-31 (0x00-0x1F) are not allowed in ntfs filenames.
+    ntfs_characters = [chr(i) for i in range(0, 31)]
+    characters = [
+        r'"',
+        r"\#",
+        r"\$",
+        r"\%",
+        r"'",
+        r"\*",
+        r"\,",
+        r"\.",
+        r"\/",
+        r"\:",
+        r'"',
+        r"\;",
+        r"\<",
+        r"\>",
+        r"\?",
+        r"\\",
+        r"\^",
+        r"\|",
+        r"\~",
+        r"\\\\",
+    ]
+    pattern = "|".join(ntfs_characters + characters)
+    regex = re.compile(pattern, re.UNICODE)
+    filename = regex.sub("", s)
+    return filename[:max_length].rsplit(" ", 0)[0]
+
+
+def download_audio_from_youtube(url, dnld_location):
+    ydl_opts = {
+        "outtmpl": dnld_location,
+        "format": "bestaudio[acodec^=mp4a]/mp4",
+    }
+
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([url])
 
 
 if __name__ == "__main__":
@@ -35,24 +89,12 @@ if __name__ == "__main__":
 
             # download audio stream
             print(f"Downloading: {search_title} {search_artist} {vid_id}")
-            yt = YouTube(f"https://www.youtube.com/watch?v={vid_id}")
-
-            # TODO: getting a weird keyerror from pytube sometimes, so just keep retrying
-            try_again = True
-            while try_again:
-                try:
-                    result = yt.streams.get_audio_only()
-                    try_again = False
-                except Exception:
-                    print("Failed, trying again...")
-
-            file_name = (
-                safe_filename(f"{search_title}_{search_artist}") + f".{result.subtype}"
-            )
-            result.download(output_path=dnld_folder, filename=file_name)
+            file_name = safe_filename(f"{search_title}_{search_artist}") + ".mp4"
+            track_file_path = os.path.join(dnld_folder, file_name)
+            track_url = f"https://www.youtube.com/watch?v={vid_id}"
+            download_audio_from_youtube(track_url, track_file_path)
 
             # verify it was downloaded
-            track_file_path = os.path.join(dnld_folder, file_name)
             if not os.path.exists(track_file_path):
                 print("TRACK DID NOT DOWNLOAD!!!!")
                 sys.exit(1)
@@ -69,5 +111,5 @@ if __name__ == "__main__":
 
             # put cover art into track
             metadata = MP4(filename=track_file_path)
-            metadata.tags["covr"] = [(MP4Cover(cover_image.content))]
+            metadata.tags["covr"] = [MP4Cover(cover_image.content)]
             metadata.save()
